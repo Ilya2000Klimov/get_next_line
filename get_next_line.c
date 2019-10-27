53c6798433756c4c6aa435169954f514c4bc2c44
Ilya Klimov <iklimov@e1z2r6p14.42.us.org>
2019-10-26 18:57:12 -0700

    october 26 6:57

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: iklimov <iklimov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 11:06:28 by iklimov           #+#    #+#             */
-/*   Updated: 2019/10/17 23:23:52 by iklimov          ###   ########.fr       */
+/*   Updated: 2019/10/26 18:56:24 by iklimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,70 @@
 
 int get_next_line(const int fd, char **line) /*return 0 when file ends, -1, 1*/
 {
-	char buff[BUFF_SIZE];
-	int bites_read;
-	char *nloc;
-	static t_list *list;
-	char static *rem[6096];
-	//char static	*str = "";
-	*line = ft_strdup(rem[fd]);
-	//check for read errors and invalid fd errors
-	//iterate through list to find the correct fd by using size as the fd (list->size = fd)  == (*list).size
-	while (read(fd, buff, BUFF_SIZE) != BUFF_SIZE)
-	{
-		buff[BUFF_SIZE] = '\0';
-		if (ft_strchr(buff, '\n'))
-			break;
-		//concatinate buff to static vars
-	}
-	//check for stuff stored in your static variables
-	//collect the correct line to store in *line
-	//remove the line given to *line from your static variable
-	//check correct return type and return it
-	while (ft_strchr(buff, '\n') == NULL)
+	char			buff[BUFF_SIZE + 1];
+	int				bites_read;
+	char			*nloc;
+	char			*tmp;
+	static t_list	*lst;
+	t_list			*tmplst;
+
+	if ((fd < 0 || line == NULL || read(fd, buff, 0) < 0))
+		return (-1);
+	tmplst = ft_list_findsize(lst, (int)fd);
+	tmp = (tmplst != NULL) ? ft_strdup(lst->content) : ft_strnew(0);
+	//free(tmplst->content);
+	bites_read = 0;
+	while(!(nloc = ft_strchr(tmp, '\n')) || nloc == NULL)
 	{
 		bites_read = read(fd, &buff, BUFF_SIZE);
-		*line = ft_strjoin(*line, buff);
+		buff[BUFF_SIZE] = '\0';
+		tmp = ft_strjoinreal(tmp, buff);
 		if (BUFF_SIZE != bites_read)
-			return (0);
+			break;
 	}
-	nloc = ft_strchr(buff, '\n');
-	ft_strjoin(*line, ft_strsub(buff, 0, (nloc - buff)));
-	//rem = ft_strsub(nloc, 0, bites_read - (nloc - buff));
-	return (1);
+	*line = ft_strsub(tmp, 0, (nloc - tmp));
+	if (tmplst == NULL)
+	{
+		tmplst = ft_lstnew("", (int)fd);
+		//ft_strdel(tmplst->content);
+		ft_lstadd(&lst, tmplst);
+	}
+	tmplst->content = ft_strsub(tmp, (nloc + 1 - tmp), bites_read - ((nloc - tmp) % BUFF_SIZE));
+	if (BUFF_SIZE != bites_read && tmplst->content == '\0')
+	{
+		ft_lstdelthis(&tmplst);
+		return 0;
+	}
+	return 1;
 }
+
+
+// 	//*line = ft_strdup(rem[fd]);
+// 	//check for read errors and invalid fd errors
+// 	//iterate through lst to find the correct fd by using size as the fd (lst->size = fd)  == (*lst).size
+// 	while (read(fd, buff, BUFF_SIZE) != BUFF_SIZE)
+// 	{
+// 		buff[BUFF_SIZE] = '\0';
+// 		if (ft_strchr(buff, '\n'))
+// 			break;
+// 		//concatinate buff to static vars
+// 	}
+// 	//check for stuff stored in your static variables
+// 	//collect the correct line to store in *line
+// 	//remove the line given to *line from your static variable
+// 	//check correct return type and return it
+// 	while (ft_strchr(buff, '\n') == NULL)
+// 	{
+// 		bites_read = read(fd, &buff, BUFF_SIZE);
+// 		*line = ft_strjoin(*line, buff);
+// 		if (BUFF_SIZE != bites_read)
+// 			return (0);
+// 	}
+// 	nloc = ft_strchr(buff, '\n');
+// 	ft_strjoin(*line, ft_strsub(buff, 0, (nloc - buff)));
+// 	//rem = ft_strsub(nloc, 0, bites_read - (nloc - buff));
+// 	return (1);
+// }
 
 // int get_next_line(const int fd, char **line)	/*return 0 when file ends, -1, 1*/
 // {
@@ -71,3 +103,23 @@ int get_next_line(const int fd, char **line) /*return 0 when file ends, -1, 1*/
 // 		return (0);
 // 	return (1);
 // }
+
+// int get_next_line(const int fd, char **line)
+// {
+// 	char buff[11];
+// 	int bytes_read;
+// 	buff[10] = '\0';
+// 	//int static i = 10;
+
+// 	bytes_read = read(fd, &buff, 10);
+// 	// while (bytes_read == 10)
+// 	// {
+// 	// 	*line = ft_strjoin(*line, buff);
+// 	// 	bytes_read = read(fd, &buff, 10);
+// 	// }
+// 	*line = ft_strdup(buff);
+// 	if (bytes_read != 10)
+// 		return 0;
+// 	return 1;
+// }
+
