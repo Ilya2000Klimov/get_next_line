aebb058df6f0cd51cc28795b59bcc4c5473d51c7
Ilya Klimov <iklimov@e1z3r5p12.42.us.org>
2019-11-02 10:24:48 -0700

    main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: iklimov <iklimov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 16:54:09 by iklimov           #+#    #+#             */
-/*   Updated: 2019/10/30 21:59:19 by iklimov          ###   ########.fr       */
+/*   Updated: 2019/11/02 10:24:20 by iklimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,6 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-// int main()
-// {
-// 	char *line;
-// 	int fd;
-// 	int fd2;
-// 	int i;
-// 	//struct stat name;
-
-// 	line = "";
-// 	// fd = open("text.txt", O_RDONLY);
-// 	fd2 = open("test2.txt", O_RDONLY);
-// 	fd = open("test1.txt", O_RDONLY);
-// 	i = 0;
-// 	while (i < 10)
-// 	{
-// 		if (get_next_line(fd, &line) > 0)
-// 		printf("f1[%s]\n", line);
-// 		if (get_next_line(fd2, &line) > 0)
-// 		printf("f2[%s]\n", line);
-// 		i++;
-// 		printf("%d\n\n", i);
-// 	}
-
-// 	// while(get_next_line(fd, &line))
-// 	// {
-// 	// 	get_next_line(fd, &line);
-// 	// 	printf("%s", line);
-// 	// 	// get_next_line(fd2, &line);
-// 	// 	// printf("%s", line);
-// 	// 	i++;
-// 	// }
-// 	//printf("%s", line);
-// 	return(0);
-// }
-
-// 	// while (i < 10)
-// 	// {
-// 	// 	if (get_next_line(fd, &line) > 0)
-// 	// 	printf("%s\n", line);
-// 	// 	if (get_next_line(fd2, &line) > 0)
-// 	// 	printf("%s\n", line);
-// 	// 	i++;
-// 	// }
-
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
@@ -97,6 +49,5 @@ int main(int argc, char **argv)
 		}
 		i++;
 	}
-	while(1);
 	return (0);
 }
